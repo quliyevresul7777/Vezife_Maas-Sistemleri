a5e68629a735cbb1b3cdf7b6b45455ea6e102fea
quliyevresul7777 <quliyev.resul7777@gmail.com>
2026-05-16 20:18:53 +0400

    Add DataStore class for managing positions, employees, and salaries

---
+++ DataStore.cs
@@ -0,0 +1,10 @@
+namespace SalaryManagementSystem;
+
+public static class DataStore
+{
+    public static List<Position> Positions = new List<Position>();
+
+    public static List<Employee> Employees = new List<Employee>();
+
+    public static List<Salary> Salaries = new List<Salary>();
+}
